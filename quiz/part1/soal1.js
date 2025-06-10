7e7867bbb700cda21f5b10b5f74860afaef96a72
DeGarde <riskiahmad655@gmail.com>
2025-06-10 17:39:10 +0700

    udah blom?

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -14,11 +14,14 @@ if (nama === "") {
     console.log("nama wajib diisi");
   } else if (peran === "") {
     console.log("Pilih Peranmu untuk memulai game");
-  } else {if (peran === "Kesatria") {
-      console.log(`halo Kesatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
-  } else if (peran === "Tabib") {
-      console.log(`halo Tabib ${nama}, kamu akan membantu temanmu yang terluka`);
-  } else if (peran === "Penyihir") {
-      console.log(`halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
-       } else {  console.log("tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada")}
+  } else {
+      if (peran === "Kesatria") {
+          console.log(`halo Kesatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
+      } else if (peran === "Tabib") {
+          console.log(`halo Tabib ${nama}, kamu akan membantu temanmu yang terluka`);
+      } else if (peran === "Penyihir") {
+          console.log(`halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
+      } else {
+          console.log("tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada");
+      }
   }
